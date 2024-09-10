32d402862a94a47b58cb69c3cbad9680b9061eb2
Amin <amin.17@gmail.com>
2024-09-11 09:17:57 +1000

    nietzsche horoscope

---
+++ tools/mobireader/horoscope.py
@@ -0,0 +1,17 @@
+import random
+import json
+
+
+with open("./resources/tsz-data.json", "r") as f:
+    fJson = json.load(f)
+
+
+uniqueNumbers = random.sample(range(1, fJson["pages"].__len__()), 3)
+
+sect = ["me", "you", "then"]
+i = 0
+
+for num in uniqueNumbers:
+    print((50 * "-") + sect[i] + " :"+str(num))
+    i+=1
+    print(fJson["pages"][num]["en"][:500])
